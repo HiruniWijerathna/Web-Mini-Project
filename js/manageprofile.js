3030b26651d807c6c778daba26f486bd096bbf6a
Sajini Weerasinghe <nssweerasinghe@std.appsc.sab.ac.lk>
2024-05-11 10:51:33 +0530

    Created manageprofile.css and manageprofile.js files in the relevent css and js folders

---
+++ js/manageprofile.js
@@ -0,0 +1,32 @@
+//uplode profile photo
+
+function previewImage(event) {
+    var preview = document.getElementById('preview');
+    var uploadInput = document.getElementById('uploadInput');
+    preview.style.display = "block";
+    preview.src = URL.createObjectURL(event.target.files[0]);
+    uploadInput.style.display = "none";
+}
+
+function removePhoto() {
+    var preview = document.getElementById('preview');
+    var uploadInput = document.getElementById('uploadInput');
+    
+    preview.src = "image/profilePhotoLogo.jpg";
+    uploadInput.value = ""; // Clear the selected file
+    uploadInput.style.display = "block";
+}
+
+// uplode name
+
+function updateText() {
+    var typedText = document.getElementById('fname').value;
+    document.getElementById('displayText').textContent = typedText;
+}
+
+function updatText() {
+    var typedText = document.getElementById('lname').value;
+    document.getElementById('displaText').textContent = typedText;
+}
+
+
