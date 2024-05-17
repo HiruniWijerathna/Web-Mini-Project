dfbe8464ff7668ffb4360a27a4233fb606764647
Hiruni Wijerathna <114440722+HiruniWijerathna@users.noreply.github.com>
2024-05-18 00:12:16 +0530

    Modified footer

--- register.php
+++ register.php
@@ -107,10 +107,12 @@
         </div>	
     </form>
 
-    <!-- Load the footer -->
-    <!-- <div id="footer"></div>
-    <script src="js/includeFooter.js"></script> -->
-    <!-- Footer -->
+    <!--...................... Footer............................................. -->
+<!-- <div id="footer"></div> -->
+
+<!-- Load Combined JavaScript -->
+<!-- <script src="js/includeContent.js"></script> -->
+<!-- Footer -->
 
 <div class="container-fluid   px-4   " style="background-color: #080433">
     <footer class="py-1">
@@ -131,10 +133,9 @@
             <div class="col-6 col-md-3 mb-5">
                 <h5 class="text-white">Follow Us On</h5>
                 <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="https://www.facebook.com/" class="nav-link p-0 text-body-secondary">Facebook</a></li>
-                    <li class="nav-item mb-2"><a href="https://www.whatsapp.com/" class="nav-link p-0 text-body-secondary">Whatsapp</a></li>
-                    <li class="nav-item mb-2"><a href="https://www.instagram.com/" class="nav-link p-0 text-body-secondary">Intagram</a></li>
-                    <li class="nav-item mb-2"><a href="https://www.youtube.com/" class="nav-link p-0 text-body-secondary">Youtube</a></li>
+                    <li class="nav-item mb-2"><a href="https://www.facebook.com/profile.php?id=61559491240781&mibextid=ZbWKwL" class="nav-link p-0 text-body-secondary">Facebook</a></li>
+                    <li class="nav-item mb-2"><a href="https://www.instagram.com/__beastbuddy__?igsh=MTZtbGp3bmhzNGk3eA==" class="nav-link p-0 text-body-secondary">Instagram</a></li>
+                    <li class="nav-item mb-2"><a href="https://youtube.com/@beastbuddy-2024?si=P8ZBuQ0NL2N8WTv6" class="nav-link p-0 text-body-secondary">Youtube</a></li>
                 </ul>
             </div>
 
@@ -146,6 +147,7 @@
                     <li class="nav-item mb-2"><a href="home.php#myCarousel" class="nav-link p-0 text-body-secondary">Home</a></li>
                     <li class="nav-item mb-2"><a href="home.php#services" class="nav-link p-0 text-body-secondary">Service</a></li>
                     <li class="nav-item mb-2"><a href="home.php#about" class="nav-link p-0 text-body-secondary">About</a></li>
+                    <li class="nav-item mb-2"><a href="home.php#help" class="nav-link p-0 text-body-secondary">Help</a></li>
                 </ul>
             </div>
 
@@ -159,8 +161,7 @@
 </div>
 
 
-
-    <!-- .................footer end............... -->
+<!-- ........................footer end ..................................-->
 
 </body>
 </html>
