e41d365f32c6c772937808fe77b5b18e0d87d878
Hiruni Wijerathna <116500366+malshikainsari@users.noreply.github.com>
2024-05-18 00:20:20 +0530

    Modified footer

--- createPost.php
+++ createPost.php
@@ -121,42 +121,62 @@ if (isset($_SESSION['username'])) {
 </div>
     <!-- End Content -->
 
-    <!-- Footer -->
-    <div class="container-fluid px-4 mt-auto" style="background-color: #080433;">
-        <footer class="py-1">
-            <div class="row flex-lg-row-align-items-center g-5 " style="background-color: #080433">
-                <div class="col-md-4 offset-md-1 mb-5">
-                    <form>
-                        <div class="text-white">
-                            <h5>BeastBuddy</h5>
-                            <p class="descrip">Dedicated to saving lives, one paw at a time. <br>Our passionate team connects animals with loving homes, making a difference in the world of animal rescue.</p>
-                        </div>
-                    </form>
-                </div>
-                <div class="col-6 col-md-3 mb-5">
-                    <h5 class="text-white">Follow Us On</h5>
-                    <ul class="nav flex-column">
-                        <li class="nav-item mb-2"><a href="https://www.facebook.com/" class="nav-link p-0 text-body-secondary">Facebook</a></li>
-                        <li class="nav-item mb-2"><a href="https://www.whatsapp.com/" class="nav-link p-0 text-body-secondary">Whatsapp</a></li>
-                        <li class="nav-item mb-2"><a href="https://www.instagram.com/" class="nav-link p-0 text-body-secondary">Instagram</a></li>
-                        <li class="nav-item mb-2"><a href="https://www.youtube.com/" class="nav-link p-0 text-body-secondary">Youtube</a></li>
-                    </ul>
-                </div>
-                <div class="col-6 col-md-2 mb-5">
-                    <h5 class="text-white">Quick Links</h5>
-                    <ul class="nav flex-column">
-                        <li class="nav-item mb-2"><a href="home.php#myCarousel" class="nav-link p-0 text-body-secondary">Home</a></li>
-                        <li class="nav-item mb-2"><a href="home.php#services" class="nav-link p-0 text-body-secondary">Service</a></li>
-                        <li class="nav-item mb-2"><a href="home.php#about" class="nav-link p-0 text-body-secondary">About</a></li>
-                    </ul>
-                </div>
-                <div class="col-12">
-                    <p class="text-center descrip">© 2024 BeastBuddy, Inc. All rights reserved.</p>
-                </div>
+    <!--...................... Footer............................................. -->
+<!-- <div id="footer"></div> -->
+
+<!-- Load Combined JavaScript -->
+<!-- <script src="js/includeContent.js"></script> -->
+<!-- Footer -->
+
+<div class="container-fluid   px-4   " style="background-color: #080433">
+    <footer class="py-1">
+        <div class="row flex-lg-row-align-items-center g-5 " style="background-color: #080433">
+
+            <div class="col-md-4 offset-md-1 mb-5">
+                <form>
+                    <div class="text-white">
+                        <h5>BeastBuddy</h5>
+                        <p class="descrip">Dedicated to saving lives, one paw at a time. <br>Our passionate team
+                            connects animals with loving homes, making a difference in the world of animal rescue.</p>
+                    </div>
+                </form>
             </div>
-        </footer>
-    </div>
-    <!-- End Footer -->
+
+            <!-- <div class="col mb-3"> </div> -->
+
+            <div class="col-6 col-md-3 mb-5">
+                <h5 class="text-white">Follow Us On</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="https://www.facebook.com/profile.php?id=61559491240781&mibextid=ZbWKwL" class="nav-link p-0 text-body-secondary">Facebook</a></li>
+                    <li class="nav-item mb-2"><a href="https://www.instagram.com/__beastbuddy__?igsh=MTZtbGp3bmhzNGk3eA==" class="nav-link p-0 text-body-secondary">Instagram</a></li>
+                    <li class="nav-item mb-2"><a href="https://youtube.com/@beastbuddy-2024?si=P8ZBuQ0NL2N8WTv6" class="nav-link p-0 text-body-secondary">Youtube</a></li>
+                </ul>
+            </div>
+
+            <!-- <div class="col mb-3"></div> -->
+
+            <div class="col-6 col-md-2 mb-5">
+                <h5 class="text-white">Quick Links</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="home.php#myCarousel" class="nav-link p-0 text-body-secondary">Home</a></li>
+                    <li class="nav-item mb-2"><a href="home.php#services" class="nav-link p-0 text-body-secondary">Service</a></li>
+                    <li class="nav-item mb-2"><a href="home.php#about" class="nav-link p-0 text-body-secondary">About</a></li>
+                    <li class="nav-item mb-2"><a href="home.php#help" class="nav-link p-0 text-body-secondary">Help</a></li>
+                </ul>
+            </div>
+
+            <!-- <div class="col mb-3"> </div> -->
+
+            <div class="d-flex flex-column flex-sm-row justify-content-center  border-top">
+                <p class="descrip">© 2024 BeastBuddy, Inc. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+</div>
+
+
+<!-- ........................footer end ..................................-->
+
 
 </body>
 </html>
