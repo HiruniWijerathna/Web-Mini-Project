ede8b03070e6fd26f5d3e5a096f1c938841f754c
Pramuditha Kavinda Bandara <166626969+KMPKBandara@users.noreply.github.com>
2024-05-18 23:25:51 +0530

    Modify viewprofile_veterinarian file

--- viewprofile_veterinarian.php
+++ viewprofile_veterinarian.php
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css\viewprofile.css">
+    <link rel="stylesheet" href="css\style.css">
 </head>
 
 <body>
@@ -51,8 +52,29 @@ if (isset($_SESSION['username'])) {
   </div>
 <!-- ......................................header end ...............................-->
 
+    <!-- carousel Section -->
+
+    <div id="myCarousel" class="carousel slide mb-6 pointer-event" data-bs-ride="carousel" style="margin-top: -0.1rem">
+       
+        <div class="carousel-inner">
+          <div class="carousel-item active hover-item" >
+            <img src="image/veterinarian_viewprofile.jpg" class="d-block w-100" alt="Image 2">
+            <div class="container">
+            <div class="carousel-caption text-start">
+                <h1 style="color: black">Veterinarian Profiles</h1>
+                <p class="opacity-75" style="color:black"><b>Discover our compassionate veterinarians dedicated to the health and well-being of rescued animals.</b></p>
+            </div>
+            </div>
+        </div>
+          
+    </div>
+   
+    </div>
+
+<!--Profile Section-->
+
     <div class="container mt-5">
-        <h2>Veterinarian Profiles</h2>
+
         <?php
         // Database connection details
         $servername = "localhost";
