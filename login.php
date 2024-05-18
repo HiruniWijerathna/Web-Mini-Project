507cbd53967270b1b0be106de68e3ebed3b47dee
Sajini Weerasinghe <nssweerasinghe@std.appsc.sab.ac.lk>
2024-05-18 14:47:57 +0530

    resolved the login issue

--- login.php
+++ login.php
@@ -4,46 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | BeastBuddy</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body class="login-body" style="background-image: url('image/loginMainImag.jpg'); background-size: cover;">
 
-    <!-- Load the header -->
-    <!-- <div id="header"></div>
-    <script src="js/includeContent.js"></script> -->
-    <!-- Header -->
- <div class="container-fluid px-4 border-bottom shadow-bottom" style="background-color: #080433">
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom ">
-      <div class="col-md-3 mb-2 mb-md-0">
-        <a href="home.php" class="d-inline-flex link-body-emphasis text-decoration-none">
-            <img src="image/logo.png" alt="Your Logo" class="logo">
-        </a>
-      </div>
+    <div class="container-fluid px-4 border-bottom shadow-bottom" style="background-color: #080433">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <div class="col-md-3 mb-2 mb-md-0">
+                <a href="home.php" class="d-inline-flex link-body-emphasis text-decoration-none">
+                    <img src="image/logo.png" alt="Your Logo" class="logo">
+                </a>
+            </div>
+            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="home.php#myCarousel" class="nav-link px-2">Home</a></li>
+                <li><a href="home.php#services" class="nav-link px-2">Services</a></li>
+                <li><a href="home.php#about" class="nav-link px-2">About</a></li>
+                <li><a href="home.php#help" class="nav-link px-2">Help</a></li>
+            </ul>
+            <div class="col-md-3 text-end">
+                <button type="button" class="btn btn-outline-primary me-4 btn-custom" onclick="window.location.href='register.php';">Register</button>
+                <button type="button" class="btn btn-primary btn-custom" onclick="window.location.href='login.php';">Login</button>
+            </div>
+        </header>
+    </div>
 
-      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="home.php#myCarousel" class="nav-link px-2 ">Home</a></li>
-        <li><a href="home.php#services" class="nav-link px-2">Services</a></li>
-        <li><a href="home.php#about" class="nav-link px-2">About</a></li>
-        <li><a href="home.php#help" class="nav-link px-2">Help</a></li>
-      </ul>
-
-      <div class="col-md-3 text-end">
-        <button type="button" class="btn btn-outline-primary me-4 btn-custom" onclick="window.location.href='register.php';">Register</button>
-        <button type="button" class="btn btn-primary btn-custom" onclick="window.location.href='login.php';">Login</button>
-      </div>
-    </header>
-  </div>
-
-
-    <!-- header end -->
-
-    <!-- login form -->
     <form id="login_form" action="login_setup.php" method="post">
-        <h1 class="login-h1">Login</h1><bR>
+        <h1 class="login-h1">Login</h1><br>
 
         <div class="form-floating">
             <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com" required>
@@ -69,68 +56,44 @@
                 <a href="https://www.instagram.com" target="_blank">
                     <img src="image/ins.png" width="45px">
                 </a>
-                <a href="https://www.twitter.com" target="_blank">	
+                <a href="https://www.twitter.com" target="_blank">  
                     <img src="image/twe.png" width="45px">
                 </a>
             </h3>
-        </div>	
+        </div>
     </form>
 
-<!--...................... Footer............................................. -->
-<!-- <div id="footer"></div> -->
-
-<!-- Load Combined JavaScript -->
-<!-- <script src="js/includeContent.js"></script> -->
-<!-- Footer -->
-
-<div class="container-fluid   px-4   " style="background-color: #080433">
-    <footer class="py-1">
-        <div class="row flex-lg-row-align-items-center g-5 " style="background-color: #080433">
-
-            <div class="col-md-4 offset-md-1 mb-5">
-                <form>
+    <div class="container-fluid px-4" style="background-color: #080433">
+        <footer class="py-1">
+            <div class="row flex-lg-row-align-items-center g-5" style="background-color: #080433">
+                <div class="col-md-4 offset-md-1 mb-5">
                     <div class="text-white">
                         <h5>BeastBuddy</h5>
-                        <p class="descrip">Dedicated to saving lives, one paw at a time. <br>Our passionate team
-                            connects animals with loving homes, making a difference in the world of animal rescue.</p>
+                        <p class="descrip">Dedicated to saving lives, one paw at a time. Our passionate team connects animals with loving homes, making a difference in the world of animal rescue.</p>
                     </div>
-                </form>
+                </div>
+                <div class="col-6 col-md-3 mb-5">
+                    <h5 class="text-white">Follow Us On</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item mb-2"><a href="https://www.facebook.com/profile.php?id=61559491240781&mibextid=ZbWKwL" class="nav-link p-0 text-body-secondary">Facebook</a></li>
+                        <li class="nav-item mb-2"><a href="https://www.instagram.com/__beastbuddy__?igsh=MTZtbGp3bmhzNGk3eA==" class="nav-link p-0 text-body-secondary">Instagram</a></li>
+                        <li class="nav-item mb-2"><a href="https://youtube.com/@beastbuddy-2024?si=P8ZBuQ0NL2N8WTv6" class="nav-link p-0 text-body-secondary">YouTube</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md-2 mb-5">
+                    <h5 class="text-white">Quick Links</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item mb-2"><a href="home.php#myCarousel" class="nav-link p-0 text-body-secondary">Home</a></li>
+                        <li class="nav-item mb-2"><a href="home.php#services" class="nav-link p-0 text-body-secondary">Service</a></li>
+                        <li class="nav-item mb-2"><a href="home.php#about" class="nav-link p-0 text-body-secondary">About</a></li>
+                        <li class="nav-item mb-2"><a href="home.php#help" class="nav-link p-0 text-body-secondary">Help</a></li>
+                    </ul>
+                </div>
             </div>
-
-            <!-- <div class="col mb-3"> </div> -->
-
-            <div class="col-6 col-md-3 mb-5">
-                <h5 class="text-white">Follow Us On</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="https://www.facebook.com/profile.php?id=61559491240781&mibextid=ZbWKwL" class="nav-link p-0 text-body-secondary">Facebook</a></li>
-                    <li class="nav-item mb-2"><a href="https://www.instagram.com/__beastbuddy__?igsh=MTZtbGp3bmhzNGk3eA==" class="nav-link p-0 text-body-secondary">Instagram</a></li>
-                    <li class="nav-item mb-2"><a href="https://youtube.com/@beastbuddy-2024?si=P8ZBuQ0NL2N8WTv6" class="nav-link p-0 text-body-secondary">Youtube</a></li>
-                </ul>
+            <div class="d-flex justify-content-between py-1 my-1 border-top" style="background-color: #080433">
+                <p style="color: white;">&copy; 2024 BeastBuddy. All rights reserved.</p>
             </div>
-
-            <!-- <div class="col mb-3"></div> -->
-
-            <div class="col-6 col-md-2 mb-5">
-                <h5 class="text-white">Quick Links</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="home.php#myCarousel" class="nav-link p-0 text-body-secondary">Home</a></li>
-                    <li class="nav-item mb-2"><a href="home.php#services" class="nav-link p-0 text-body-secondary">Service</a></li>
-                    <li class="nav-item mb-2"><a href="home.php#about" class="nav-link p-0 text-body-secondary">About</a></li>
-                    <li class="nav-item mb-2"><a href="home.php#help" class="nav-link p-0 text-body-secondary">Help</a></li>
-                </ul>
-            </div>
-
-            <!-- <div class="col mb-3"> </div> -->
-
-            <div class="d-flex flex-column flex-sm-row justify-content-center  border-top">
-                <p class="descrip">© 2024 BeastBuddy, Inc. All rights reserved.</p>
-            </div>
-        </div>
-    </footer>
-</div>
-
-
-<!-- ........................footer end ..................................-->
-
+        </footer>
+    </div>
 </body>
 </html>
