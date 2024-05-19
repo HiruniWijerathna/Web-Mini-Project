7ebd06d23585ded5474e326aeec36ddcd7482a43
Hiruni Wijerathna <114440722+HiruniWijerathna@users.noreply.github.com>
2024-05-19 20:30:46 +0530

    modified

--- viewprofile_animalorganization.php
+++ viewprofile_animalorganization.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css\viewprofile.css">
     <link rel="stylesheet" href="css\style.css">
-    <style>
+    
 
 </head>
 
@@ -64,7 +64,7 @@ if (isset($_SESSION['username'])) {
            <img src="image/AnimalOrganization.jpg" class="d-block w-100" alt="Image 2">
            <div class="container">
            <div class="carousel-caption text-start">
-               <h1 style="color: black">Animal Organizations</h1>
+               <h1 style="color: White">Animal Organizations Profiles</h1>
                <p class="opacity-75" style="color:white">Explore our committed animal organizations working tirelessly to rescue and protect animals in need.</p>
            </div>
            </div>
@@ -186,5 +186,7 @@ if (isset($_SESSION['username'])) {
 
 
 </body>
+
+
 
 </html>
