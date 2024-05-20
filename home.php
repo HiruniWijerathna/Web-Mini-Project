f47ee04dfc3da1ceae7844e48150ccbb422c203e
Hiruni Wijerathna <114440722+HiruniWijerathna@users.noreply.github.com>
2024-05-21 00:22:52 +0530

    Home page modified

--- home.php
+++ home.php
@@ -86,54 +86,58 @@
         </button>
       </div>
 
-  <!-- serveces section -->
+  <!-- services section --><br>
 <div id="services" class="container-fluid py-5">
-    <h1 class="display-5 fw-bold text-body-emphasis" style="text-align: center;">Services</h1>
-
-    <div class="row">
-        <div class="col-lg-3 service-item">
+    <h1 class="display-5 fw-bold text-body-emphasis" style="text-align: center;">Services</h1><br>
+    <div class="d-flex justify-content-center flex-wrap">
+        <div class="col-lg-2 col-md-3 col-sm-4 service-item text-center m-3">
             <svg class="bd-placeholder-img rounded-circle" width="220" height="220" xmlns="http://www.w3.org/2000/svg"
                 role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
                 <rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
                 <image href="image\ReadPost.jpeg" width="220" height="220" fill="var(--bs-secondary-color)" />
             </svg>
-            <h2 class="fw-normal " ></h2>
+            
             <p><a class="btn btn-secondary" href="readpost2.php">Read post »</a></p>
-
-        </div><!-- /.col-lg-3 -->
-        <div class="col-lg-3 service-item">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 service-item text-center m-3">
             <svg class="bd-placeholder-img rounded-circle" width="220" height="220" xmlns="http://www.w3.org/2000/svg"
                 role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
                 <rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
                 <image href="image\vet.jpeg" width="220" height="220" fill="var(--bs-secondary-color)" />
             </svg>
-            <h2 class="fw-normal"></h2>
+            
             <p><a class="btn btn-secondary" href="login.php">Veterinary advice »</a></p>
-        </div><!-- /.col-lg-3 -->
-        <div class="col-lg-3 service-item">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 service-item text-center m-3">
             <svg class="bd-placeholder-img rounded-circle" width="220" height="220" xmlns="http://www.w3.org/2000/svg"
                 role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
                 <rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
                 <image href="image\animalOrganization.jpeg" width="220" height="220" fill="var(--bs-secondary-color)" />
             </svg>
-            <h2 class="fw-normal"></h2>
+            
             <p><a class="btn btn-secondary" href="login.php">Animal Organization »</a></p>
-        </div><!-- /.col-lg-3 -->
-        <div class="col-lg-3 service-item">
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 service-item text-center m-3">
             <svg class="bd-placeholder-img rounded-circle" width="220" height="220" xmlns="http://www.w3.org/2000/svg"
                 role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
                 <rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
                 <image href="image\snakeinfor.jpeg" width="220" height="220" fill="var(--bs-secondary-color)" />
             </svg>
-            <h2 class="fw-normal"></h2>
+            
             <p><a class="btn btn-secondary" href="login.php">Snake Information »</a></p>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 service-item text-center m-3">
+            <svg class="bd-placeholder-img rounded-circle" width="220" height="220" xmlns="http://www.w3.org/2000/svg"
+                role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
+                <image href="image/AnimalSearch&RescueLogo.jpg" width="220" height="220" fill="var(--bs-secondary-color)"/>
+            </svg>
+           
+            <p><a class="btn btn-secondary" href="login.php">Animal Search & Rescue »</a></p>
         </div>
     </div>
 </div>
+
 
 <!-- About -->
 <div id="about" class="px-4 py-5 my-5 text-center">
