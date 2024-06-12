3eea22d49379b31e3b785719036d6058fa7c73dd
Pramuditha Kavinda Bandara <166626969+KMPKBandara@users.noreply.github.com>
2024-06-12 13:02:44 +0530

    Remove Second session_start():

--- viewprofile_snakecatcher.php
+++ viewprofile_snakecatcher.php
@@ -39,7 +39,6 @@ $result = $stmt->get_result();
 <!-- Add your header here -->
 <!--.......................... Header................................ -->
 <?php
-session_start(); // Start the session
 
 // Check if the username is set in the session
 if (isset($_SESSION['username'])) {
