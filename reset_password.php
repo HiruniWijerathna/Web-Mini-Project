b9f24309d743455a2b73f5c31c65f1fb6516b011
Hiruni Wijerathna <114440722+HiruniWijerathna@users.noreply.github.com>
2024-06-16 21:48:47 +0530

    header add

--- reset_password.php
+++ reset_password.php
@@ -1,4 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+</head>
+<body>
+      <!--.......................... Header................................ -->
+   <!-- <div id="header"></div> -->
+   <!-- Header -->
+ <div class="container-fluid px-4 border-bottom shadow-bottom" style="background-color: #080433">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom ">
+      <div class="col-md-3 mb-2 mb-md-0">
+        <a href="home.php" class="d-inline-flex link-body-emphasis text-decoration-none">
+            <img src="image/logo.png" alt="Your Logo" class="logo">
+        </a>
+      </div>
+
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="home.php#myCarousel" class="nav-link px-2 ">Home</a></li>
+        <li><a href="home.php#services" class="nav-link px-2">Services</a></li>
+        <li><a href="home.php#about" class="nav-link px-2">About</a></li>
+        <li><a href="home.php#help" class="nav-link px-2">Help</a></li>
+      </ul>
+
+      <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-4 btn-custom" onclick="window.location.href='register.php';">Register</button>
+        <button type="button" class="btn btn-primary btn-custom" onclick="window.location.href='login.php';">Login</button>
+      </div>
+    </header>
+  </div>
+<!-- ......................................header end ...............................-->
+</body>
+</html>
+
 <?php
+
 date_default_timezone_set('America/New_York'); // Set your desired timezone
 
 require 'config.php';
@@ -77,8 +119,8 @@ if (isset($_GET['token'])) {
             </div>
         </div>
     </div>
-<br> <br> <br> <br> <br> <br>
 
+<br> <br> <br> <br> <br> <br>
     <!--...................... Footer............................................. -->
 <!-- <div id="footer"></div> -->
 
