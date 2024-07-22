2a99bca4b02ca9e2150ed87413fcf3388522854b
Sajini Weerasinghe <nssweerasinghe@std.appsc.sab.ac.lk>
2024-07-23 00:34:37 +0530

    update profiles

--- viewprofile_snakecatcher.php
+++ viewprofile_snakecatcher.php
@@ -82,6 +82,8 @@ $result = $stmt->get_result();
                 <div class="col-md-4 mb-3">
                     <div class="card border-primary">
                         <div class="card-body">
+                        <h5 class="card-title mt-3"><?php echo htmlspecialchars($row['first_name']) . ' ' . htmlspecialchars($row['last_name']); ?></h5>
+
                             <?php
                             if (!empty($row['profile_image'])) {
                                 $imageData = base64_encode($row['profile_image']);
@@ -91,8 +93,9 @@ $result = $stmt->get_result();
                                 echo "<img src='image/profilePhotoLogo.jpg' class='img-fluid' alt='Profile Image' style='width: 100%; height: auto;'>";
                             }
                             ?>
-                            <h5 class="card-title mt-3"><?php echo htmlspecialchars($row['first_name']) . ' ' . htmlspecialchars($row['last_name']); ?></h5>
-                            <p class="card-text">Experience: <?php echo htmlspecialchars($row['experience']); ?></p>
+                            <!-- <h5 class="card-title mt-3"><?php echo htmlspecialchars($row['first_name']) . ' ' . htmlspecialchars($row['last_name']); ?></h5> -->
+                            <!-- <p class="card-text">Experience: <?php echo htmlspecialchars($row['experience']); ?></p> -->
+                             <br><br>
                             <button class="btn btn-primary read-more-btn" 
                                 data-bs-toggle="modal" 
                                 data-bs-target="#profileModal" 
@@ -169,6 +172,62 @@ document.addEventListener('hidden.bs.modal', function (event) {
     location.reload();
 });
 </script>
+
+    <!--...................... Footer............................................. -->
+<!-- <div id="footer"></div> -->
+
+<!-- Load Combined JavaScript -->
+<!-- <script src="js/includeContent.js"></script> -->
+<!-- Footer -->
+
+<div class="container-fluid   px-4   " style="background-color: #080433">
+    <footer class="py-1">
+        <div class="row flex-lg-row-align-items-center g-5 " style="background-color: #080433">
+
+            <div class="col-md-4 offset-md-1 mb-5">
+                <form>
+                    <div class="text-white">
+                        <h5>BeastBuddy</h5>
+                        <p class="descrip">Dedicated to saving lives, one paw at a time. <br>Our passionate team
+                            connects animals with loving homes, making a difference in the world of animal rescue.</p>
+                    </div>
+                </form>
+            </div>
+
+            <!-- <div class="col mb-3"> </div> -->
+
+            <div class="col-6 col-md-3 mb-5">
+                <h5 class="text-white">Follow Us On</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="https://www.facebook.com/profile.php?id=61559491240781&mibextid=ZbWKwL" class="nav-link p-0 text-body-secondary">Facebook</a></li>
+                    <li class="nav-item mb-2"><a href="https://www.instagram.com/__beastbuddy__?igsh=MTZtbGp3bmhzNGk3eA==" class="nav-link p-0 text-body-secondary">Instagram</a></li>
+                    <li class="nav-item mb-2"><a href="https://youtube.com/@beastbuddy-2024?si=P8ZBuQ0NL2N8WTv6" class="nav-link p-0 text-body-secondary">Youtube</a></li>
+                </ul>
+            </div>
+
+            <!-- <div class="col mb-3"></div> -->
+
+            <div class="col-6 col-md-2 mb-5">
+                <h5 class="text-white">Quick Links</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="home.php#myCarousel" class="nav-link p-0 text-body-secondary">Home</a></li>
+                    <li class="nav-item mb-2"><a href="home.php#services" class="nav-link p-0 text-body-secondary">Service</a></li>
+                    <li class="nav-item mb-2"><a href="home.php#about" class="nav-link p-0 text-body-secondary">About</a></li>
+                    <li class="nav-item mb-2"><a href="home.php#help" class="nav-link p-0 text-body-secondary">Help</a></li>
+                </ul>
+            </div>
+
+            <!-- <div class="col mb-3"> </div> -->
+
+            <div class="d-flex flex-column flex-sm-row justify-content-center  border-top">
+                <p class="descrip">© 2024 BeastBuddy, Inc. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+</div>
+
+
+<!-- ........................footer end ..................................-->
 
 </body>
 </html>
